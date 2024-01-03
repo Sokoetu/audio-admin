bf4fad6ad2238b2c2eb2f3794c060ac98d3dd615
kesh25 <kinyua.dn@outlook.com>
2024-01-03 11:28:55 +0300

    admin update

--- src/components/auth/authentication.tsx
+++ src/components/auth/authentication.tsx
@@ -10,13 +10,16 @@ import { getUser } from '@/functions/api-calls/user';
 import { createToast } from '@/functions/toast';
 
 const Authenticate = ({}) => {
+    const [mounted, setMounted] = React.useState(false);
     const auth = useAuthUser(); 
     const {push} = useRouter(); 
     const user = auth(); 
 
-    useCustomEffect(() => {fetchUser()}, [user]); 
+    useCustomEffect(() => {setMounted(true)}, []); 
+    useCustomEffect(() => {fetchUser()}, [mounted]); 
 
     const fetchUser = async () => {
+        if (!mounted) return
         if (!user) {
             createToast("error", "You have to be logged in to access the section!");
             push("/login")
--- src/functions/api-calls/user.js
+++ src/functions/api-calls/user.js
@@ -2,16 +2,16 @@ import {getDoc, patchDoc} from '@/utils/api-calls';
 
 // get user
 export const getUser = async (log = true) => {
-    let res = await getDoc(`/users/admin`, log) ;
+    let res = await getDoc(`/users/admin?type=admin`, log) ;
     return res?.data || false
 }
 
 export const updateUser = async (data) => {
-    let res = await patchDoc(`/users/admin`, data, true);
+    let res = await patchDoc(`/users/admin?type=admin`, data, true);
     return res?.status === 'success' || false; 
 }
 
 export const updatePassword = async (data) => {
-    let res = await patchDoc(`/users/password/admin`, data, true);
+    let res = await patchDoc(`/users/password/admin?type=admin`, data, true);
     return res?.status === 'success' || false; 
 }
